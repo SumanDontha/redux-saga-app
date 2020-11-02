5350231bd32254a5672654bd32907cc53dc7578c
Suman Dontha <dontha.suman@gmail.com>
2020-11-02 11:25:19 +0000

    Created basic structure of redux-reatc.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Users from './components/UsersComponent'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Users />
+    </div>
+  );
+}
+
+export default App;
